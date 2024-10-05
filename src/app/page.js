33e73b79947c7c7b25576840ea850e9fb63432cd
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-10-06 01:57:11 +0700

    feat: login to github

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,20 @@
-export default function Home() {
+import { signIn, signOut } from '../../auth';
+import { auth } from '../../auth';
+
+export default async function Home() {
+  const session = await auth();
+  console.log(session);
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       Others are coming soon
-      <span className={'tét'}></span>
+      <form
+        action={async () => {
+          'use server';
+          await signIn('github');
+        }}
+      >
+        <button type="submit">Signin with GitHub</button>
+      </form>
     </div>
   );
 }
